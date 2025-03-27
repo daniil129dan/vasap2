aa4d0ca14723c7b953f8e4f18768fb6c98b55e27
daniil129dan <daniil129dan@gmail.com>
2025-03-27 20:54:56 +0300

    docs fix

--- docs/conf.py
+++ docs/conf.py
@@ -5,16 +5,14 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))  # Чтобы Sphinx видел Django-проект
 
-# Добавьте эти расширения (если их нет)
-extensions = [
-    'sphinx.ext.autodoc',  # Автодокументация Python-кода
-    'sphinx.ext.viewcode',  # Добавляет ссылки на исходный код
-]
+
+
+sys.path.insert(0, os.path.abspath(os.path.join('..', 'djangotutorial')))
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 project = 'djangotutor'
 copyright = '2025, daniil'
@@ -23,6 +21,21 @@ release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autodoc',
+    'sphinx_autodoc_typehints',  # для автогенерации подсказок типов
+    'sphinxcontrib_django',  # для поддержки Django
+]
+autodoc_member_order = 'bysource'  # Порядок следования членов по исходному коду
+autodoc_default_options = {
+    'members': True,  # Включает все члены класса
+    'undoc-members': True,  # Включает даже не задокументированные члены
+    'show-inheritance': True,  # Показывает наследование классов
+}
+
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
